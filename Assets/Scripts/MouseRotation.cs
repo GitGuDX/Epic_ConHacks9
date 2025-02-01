5c6270713ba00fa8323b7e5f9d665490ff8e8bd2
Victor Iliescu <58787321+GitGuDX@users.noreply.github.com>
2025-02-01 15:02:57 -0500

    added mouse rotation

---
+++ Assets/Scripts/MouseRotation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTowardsMouse();
+    }
+
+    private void RotateTowardsMouse()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Vector3 targetPosition = hitInfo.point;
+            targetPosition.y = transform.position.y; // Keep the player on the same height
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+        }
+    }
+}

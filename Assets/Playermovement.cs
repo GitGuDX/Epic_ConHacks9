121e01df5cd16cd24fbce084e3b15f219f1fe3bd
Nichorito <nsheppard999@gmail.com>
2025-02-01 13:38:42 -0500

    Add basic sprint and dodge mechanic

--- Assets/Playermovement.cs
+++ Assets/Playermovement.cs
@@ -1,18 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
-public class Playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float sprintSpeed = 8f;
+    public float dodgeSpeed = 10f;
+    public float dodgeDuration = 0.2f;
     private Rigidbody rb;
+    private bool isDodging = false;
+    private Vector3 originalScale;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        originalScale = transform.localScale;
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        if (!isDodging)
+        {
+            MovePlayer();
+        }
     }
 
     private void MovePlayer()
@@ -20,7 +31,34 @@ public class Playermovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+
+        Vector3 move = new Vector3(moveX, 0, moveZ) * currentSpeed * Time.deltaTime;
         rb.MovePosition(transform.position + move);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Dodge(move));
+        }
+    }
+
+    private IEnumerator Dodge(Vector3 direction)
+    {
+        isDodging = true;
+        Vector3 dodgeMove = direction.normalized * dodgeSpeed;
+        float startTime = Time.time;
+
+        // Halve the player's height
+        transform.localScale = new Vector3(originalScale.x, originalScale.y / 2, originalScale.z);
+
+        while (Time.time < startTime + dodgeDuration)
+        {
+            rb.MovePosition(transform.position + dodgeMove * Time.deltaTime);
+            yield return null;
+        }
+
+        // Reset the player's height
+        transform.localScale = originalScale;
+        isDodging = false;
     }
 }

c6360bf62a968e1f39c468fb1ded076ce212cf81
spevnev <spevnev16@gmail.com>
2025-02-02 08:08:20 -0500

    Add main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,11 @@
-using Fusion;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Canvas canvas;
-    public NetworkRunner runner;
-
     public void PlayGame()
     {
-        Debug.Log("fuck");
-        canvas.gameObject.SetActive(false);
-        runner.StartGame(new StartGameArgs {
-            EnableClientSessionCreation = true,
-            PlayerCount = 2,
-            IsOpen = true,
-            IsVisible = true,
-        });
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()

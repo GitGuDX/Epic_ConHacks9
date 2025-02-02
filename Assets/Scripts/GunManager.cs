840ff9fe1761f8003ff078e5d3075422a0249086
Nichorito <nsheppard999@gmail.com>
2025-02-01 20:09:42 -0500

    Add pickupable guns

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -2,22 +2,43 @@ using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
-    public GameObject gunPrefab; // Assign AK-47 model in inspector
-    public Transform gunHolster; // Assign Gun Holster object in inspector
+    public GameObject gunPrefab;
+    public Transform gunHolster;
     private GameObject currentGun;
 
     void Start()
     {
-        SpawnGun();
+       // if (gunPrefab != null)
+        //    SpawnGun(gunPrefab);
     }
 
-    void SpawnGun()
+    // OnTriggerEnter is called when the Player collider enters the trigger
+    void OnTriggerEnter(Collider other)
     {
-        // Spawn gun as child of holster
-        currentGun = Instantiate(gunPrefab, gunHolster.position, gunHolster.rotation);
-        currentGun.transform.SetParent(gunHolster);
+        Pickupable pickupable = other.GetComponent<Pickupable>();
+        if (pickupable != null)
+        {
+            // Destroy current gun if exists
+            if (currentGun != null)
+                Destroy(currentGun);
 
-        // Set local position and rotation to align with holster
+            // Spawn new gun
+            SpawnGun(pickupable.gunPrefab);
+
+            // Destroy pickup
+            Destroy(other.gameObject);
+
+            // Free spawn point
+            ItemSpawner spawner = FindFirstObjectByType<ItemSpawner>();
+            if (spawner != null)
+                spawner.FreeSpawnPoint(other.transform.parent);
+        }
+    }
+
+    void SpawnGun(GameObject gunToSpawn)
+    {
+        currentGun = Instantiate(gunToSpawn, gunHolster.position, gunHolster.rotation);
+        currentGun.transform.SetParent(gunHolster);
         currentGun.transform.localPosition = Vector3.zero;
         currentGun.transform.localRotation = Quaternion.identity;
     }
---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    public GameObject gunPrefab;
+}

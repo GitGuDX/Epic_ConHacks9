f2ee2f7a5c1ba9c5b13eab4809e2168c7a4b78ee
spevnev <spevnev16@gmail.com>
2025-02-02 07:50:59 -0500

    Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,21 @@
+using Fusion;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Canvas canvas;
+    public NetworkRunner runner;
+
     public void PlayGame()
     {
+        Debug.Log("fuck");
         canvas.gameObject.SetActive(false);
+        runner.StartGame(new StartGameArgs {
+            EnableClientSessionCreation = true,
+            PlayerCount = 2,
+            IsOpen = true,
+            IsVisible = true,
+        });
     }
 
     public void QuitGame()

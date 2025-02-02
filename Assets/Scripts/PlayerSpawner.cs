2abff10a87a685962458c1edf5a96fffec8bf88e
spevnev <spevnev16@gmail.com>
2025-02-02 01:57:52 -0500

    Map changes

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject PlayerPrefab;
+    public Transform playerOneSpawn;
+    public Transform playerTwoSpawn;
+
+    [Networked]
+    private bool isFirstPlayer { get; set; } = false;
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+            Runner.Spawn(PlayerPrefab, isFirstPlayer ? playerOneSpawn.position : playerTwoSpawn.position, Quaternion.identity);
+            isFirstPlayer = !isFirstPlayer;
         }
     }
 }
